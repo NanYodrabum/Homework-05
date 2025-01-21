f31f6dbc2c8d8949a0b72793d4d119ca3e818324
NanYodrabum <nanyodrabum@gmail.com>
2025-01-21 09:53:10 +0700

    start with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-pink-300 hover:underline">
+        Codecamp19
+      </h1>
+    </>
+  )
+}
+
+export default App
